b21ef6ce46461aaad319acd54f29f12d9da158a3
nicholasrea <n.rea08@gmail.com>
2023-01-02 17:00:03 -0500

    Express server added

---
+++ Server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// Set up a route to respond to GET requests at the root path
+app.get('/', function (req, res) {
+  res.send('Hello, world!');
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Server listening on port ' + port);
+});
