fa4ae029fa6ebec2e756a0cac80673cec2ff6494
nicholasrea <n.rea08@gmail.com>
2023-01-19 13:07:39 -0500

    FIX Transaction prop wrong label

--- client/src/componets/Home.jsx
+++ client/src/componets/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
   if (token)
     return (
       <Transaction
-        id="withdraw"
+        id="deposit"
         placeholder="Deposit"
         opp="Withdraw"
         link="/withdraw"
