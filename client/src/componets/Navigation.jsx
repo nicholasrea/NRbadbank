2a6b80de41b5f5e9b0609036d273d39ffc031d28
nicholasrea <n.rea08@gmail.com>
2023-01-19 12:26:11 -0500

    NAV UI Update

--- client/src/componets/Navigation.jsx
+++ client/src/componets/Navigation.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
-import AuthContext from '../auth/AuthContext';
-import { Link, NavLink } from 'react-router-dom';
+import React, { useState, useContext, useEffect } from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import AuthContext from "../auth/AuthContext";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../assets/Images/bblogo.png";
 
 //TODO: ADD Logout functionality
 
@@ -9,35 +10,39 @@ export default function Navigation() {
   const { user, signOut } = useContext(AuthContext);
   const [currentUser, setCurrentUser] = useState(false);
 
-  // Flip flag, to trigger a refresh if user is updated. 
+  // Flip flag, to trigger a refresh if user is updated.
   useEffect(() => {
-    user ? setCurrentUser (true) : setCurrentUser(false);
-  }, [user] )
+    user ? setCurrentUser(true) : setCurrentUser(false);
+  }, [user]);
 
-    
   return (
     <Navbar bg="light" expand="lg">
       <Navbar.Brand>
-        <Link to="/">BadBank</Link>
+        <Link to="/">
+          <img src={logo} alt="badbank" width="50" height="50" />
+        </Link>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <NavLink to="/login" className="nav-link">
+          {(!currentUser)  && <NavLink to="/login" className="nav-link">
             Log In
-          </NavLink>
-          <NavLink to="/deposit" disabled={!user} className="nav-link">
+          </NavLink>}
+          <NavLink to="/deposit" disabled={!currentUser} className="nav-link">
             Deposit
           </NavLink>
-          <NavLink to="/withdraw" disabled={!user} className="nav-link">
+          <NavLink to="/withdraw" disabled={!currentUser} className="nav-link">
             Withdraw
-          </NavLink>                         
+          </NavLink>
         </Nav>
       </Navbar.Collapse>
       <Navbar.Text>
-        {user && <div>Signed in as: <a onClick={signOut}>{user.email}</a></div>}
+        {user && (
+          <div>
+            Signed in as: <a onClick={signOut}>{user.email}</a>
+          </div>
+        )}
       </Navbar.Text>
     </Navbar>
   );
-};
-
+}
